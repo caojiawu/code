b691cd72e0557cc36a77677ddba9abea518c1670
caojiaweu <caojiawu@wo.cn>
2019-07-22 06:55:17 +0000

    java test

---
+++ java/TClass.java
@@ -0,0 +1,26 @@
+package test.basic;
+
+public class TClass<T>{
+	private T x;
+	private T y;
+	public void setX(T x){
+		this.x = x;
+	}	
+	public void setY(T y){
+		this.y = y;
+	}
+	public T getX(){
+		return this.x;
+	}
+	public T getY(){
+		return this.y;
+	}
+
+	public static void main(String[] args){
+		TClass<Integer> a = new TClass<Integer>();
+		a.setX(new Integer(args[0]));
+		a.setY(new Integer(args[1]));
+		System.out.println(a.getX());
+		System.out.println(a.getY());	
+	};
+}
