60d842bce742f48ed3b8dd7313ebcfc0227ccc62
caojiawu <caojiawu@wo.cn>
2019-07-23 15:00:53 +0800

    none

--- java/basic/TestJSON.java
+++ java/basic/TestJSON.java
@@ -1,8 +1,20 @@
 package basic;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class TestJSON {
     public static void main(String[] args) {
+        String jsonStr = "{'name':'xiaoming','age':12,'class':[{'name':'math'},{'name':'english'}]}";
+        JSONObject jsonObject = new JSONObject(jsonStr);
+        System.out.println(jsonObject.get("name"));
+
+        JSONArray jsonArray = (JSONArray)jsonObject.get("class");
+
+        for(int i=0;i<jsonArray.length();i++){
+            JSONObject jsonObject1 = jsonArray.getJSONObject(i);
+            System.out.println(jsonObject1.get("name"));
+
+        }
 
     }
 }
