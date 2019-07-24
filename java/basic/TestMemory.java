1dc23f1b21b2f4b45da8b55ebb56b7d4cfd1dbe3
caojiawu <caojiawu@wo.cn>
2019-07-24 10:07:43 +0800

    none

--- java/basic/TestMemory.java
+++ java/basic/TestMemory.java
@@ -3,16 +3,29 @@ package basic;
 public class TestMemory {
     public static void main(String[] args){
         testMemoryAllocate();
+
     }
 
     //bullshit memory allocation mechanism, this lead to confusion.
     public static void testMemoryAllocate(){
         Integer a1 = 33;
         Integer a2 = 33;
-        System.out.println(a1==a2); //Integer类型取值在[-128,127]范围，内存为中常量池分配
+        System.out.println(a1==a2); //True. Integer类型取值在[-128,127]范围，常量池中分配并查找一个已经存在的对象
 
         Integer b1 = 330;
         Integer b2 = 330;
-        System.out.println(b1==b2); //Integer类型取值不在[-128,127]范围，对象为在堆中分配
+        System.out.println(b1==b2); //False. Integer类型取值不在[-128,127]范围，常量池中直接创建一个新的对象
+
+        int c1 = 330;
+        int c2 = 330;
+        System.out.println(c1==c2); //True.
+
+        String d1 = "hello world";
+        String d2 = "hello world";
+        System.out.println(d1==d2); //True. 对于在常量池中分配的字符串，直接查找已经存在的对象并返回.
+
+        String e1 = new String("hello world");
+        String e2 = new String("hello world");
+        System.out.println(e1==e2); //False. 在堆上分配的对象，每次地址均不同.
     }
 }
