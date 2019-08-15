dc462f2ea466341e3e1af9cb30efd42bbae7f267
caojiawu <caojiawu@wo.cn>
2019-08-15 19:10:09 +0800

    none

---
+++ MyAdriod/app/src/main/java/sample/ConstantClass.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class ConstantClass {
+    final public static int CANVAS_WIDTH = 37;
+    final public static int CANVAS_HEIGHT = 30;
+    final public static int GRID_WIDTH = 15;
+    final public static int GRID_RADIUS = 8;
+    final public static int BASE_X = 10;
+    final public static int BASE_Y = 10;
+    final public static int BEANS_COUNT = 100;
+}
---
+++ MyAdriod/app/src/main/java/sample/NewMain.java
@@ -0,0 +1,10 @@
+package sample;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class NewMain {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
